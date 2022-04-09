0afcd1737223cad33f5cc1e79b29af2a47c43ef1
Igor Khodyko <Igor_Khodyko@epam.com>
2022-04-09 16:39:02 +0300

    comment

--- src/main/java/com/example/shop/ShopApplication.java
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -9,8 +9,8 @@ import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
 
 /**
- * It is module of multi-module project "Shop"
- * This module is used for authorization and authentication users
+ * It is module of multi-module project "Shop".
+ * This module is used for authorization and authentication of users.
  * For start-loading DB Liquibase is used.
  *
  * @author Khodyko Igor
